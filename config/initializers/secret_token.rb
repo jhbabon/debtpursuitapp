a273c008632bdc1470aef0e78518d1ed1c922788
Juan Hernández Babón <juan.hernandez.babon@gmail.com>
2010-11-10 20:20:23 +0100

    Initial configuration
    Added the following gems:
    
    * pg, for the PostgreSQL database.
    * factory_girl_rails, for the test fixtures.
    * rails3-generators, for the Factory Girl generators.
    * jquery-rails, to use jQuery instead of Prototype.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DebtPursuit::Application.config.secret_token = '6aaca6a4256c047073ed71809b64376d064b7746863162c52627dca071fc8e7356c7ba0c69ca4c916246c94a97c1e484e304dd70e327155865660a7beb0843d2'
